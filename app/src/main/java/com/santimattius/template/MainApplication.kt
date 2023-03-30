c6932c1b9e7dd62417fef51c546cc36c2eb63063
Santiago Mattiauda <santimattius@gmail.com>
2023-03-30 20:43:00 -0300

    feature: implement koin annotations

--- app/src/main/java/com/santimattius/template/MainApplication.kt
+++ app/src/main/java/com/santimattius/template/MainApplication.kt
@@ -1,7 +1,20 @@
 package com.santimattius.template
 
 import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
+import com.santimattius.template.di.AppModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+import org.koin.ksp.generated.*
 
-@HiltAndroidApp
-class MainApplication : Application()
+class MainApplication : Application(){
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(AppModule().module)
+        }
+    }
+}
